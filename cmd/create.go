56159d68ff941077e7f31fd490810912385bcab6
Gunnar Jansen <gunnar.jansen@unibe.ch>
2023-05-26 20:45:30 +0200

    Added initial working create command

---
+++ cmd/create.go
@@ -0,0 +1,294 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"io/ioutil"
+	"bytes"
+        "os"
+	"path/filepath"
+        "strings"
+        "regexp"
+        "text/template"
+        "github.com/AlecAivazis/survey/v2"
+        "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "create",
+	Short: "create ansible role from template",
+	Long: `
+Initialize a new ansible role from a template repository. 
+Different platforms for github actions and molecule testing 
+are supported and automatically configured.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Creating a new role from template. Let's get started!")
+		createRole()
+	},
+}
+
+// the questions to ask
+var qs = []*survey.Question{
+    {
+        Name:     "name",
+        Prompt:   &survey.Input{Message: "What is your role called?"},
+        Validate: survey.Required,
+    },
+    {
+        Name:     "author",
+        Prompt:   &survey.Input{Message: "What is your name?"},
+        Validate: survey.Required,
+    },
+    {
+        Name:     "company",
+        Prompt:   &survey.Input{Message: "What is your company?"},
+        Validate: survey.Required,
+    },
+    {
+        Name:     "namespace",
+        Prompt:   &survey.Input{Message: "Which namespace controls the role?"},
+        Validate: survey.Required,
+    },
+    {
+    	Name: "platforms",
+    	Prompt: &survey.MultiSelect{
+    		Message: "Choose one or more platform:",
+    		Options: []string{
+    			"ubuntu2004",
+    			"ubuntu2204",
+    			"rockylinux8",
+    			"rockylinux9",
+    		},
+    	},
+    },
+    {
+        Name: "license",
+        Prompt: &survey.Select{
+            Message: "Choose a license:",
+            Options: []string{"GPL-2.0-or-later", "GPL-3.0-or-later", "MIT", "BSD"},
+            Default: "MIT",
+        },
+    },
+    {
+        Name:     "description",
+        Prompt:   &survey.Input{Message: "Please enter a short role description."},
+        Validate: survey.Required,
+    },
+}
+
+type Metadata struct {
+        Name string
+        Description string
+        Company string
+        License string
+        Author string
+        Namespace string
+        Platforms []string
+}
+
+// CheckIfError should be used to naively panics if an error is not nil.
+func CheckIfError(err error) {
+	if err == nil {
+		return
+	}
+
+	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
+	os.Exit(1)
+}
+
+// Info should be used to describe the example commands that are about to run.
+func Info(format string, args ...interface{}) {
+	fmt.Printf("\x1b[34;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
+}
+
+// Warning should be used to display a warning
+func Warning(format string, args ...interface{}) {
+	fmt.Printf("\x1b[36;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
+}
+
+// confirm function asks for user input
+// returns bool
+func confirm() bool {
+
+	var input string
+
+	fmt.Printf("Do you want to continue with this operation? [y|n]: ")
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		panic(err)
+	}
+	input = strings.ToLower(input)
+
+	if input == "y" || input == "yes" {
+		return true
+	}
+	return false
+
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func createRole() {
+
+        // the answers will be written to this struct
+        meta := Metadata{}
+    
+        // perform the questions
+        err := survey.Ask(qs, &meta)
+        if err != nil {
+            fmt.Println(err.Error())
+            return
+        }
+        fmt.Printf("%v\n", meta)
+    
+        fmt.Println("Please check you inputs. Is everything correct?")
+        if !confirm() {
+        	fmt.Println("Aborting")
+		return
+        }
+
+
+	// URL of the repository to clone
+
+        repoURL := "git@github.com:hpc-unibe-ch/ansible-role-template.git"
+	// Destination directory for cloning the repository
+	destDir := "/tmp/repo"
+
+	// Clone the repository to the destination directory
+	_, err = git.PlainClone(destDir, false, &git.CloneOptions{
+		URL:  repoURL,
+                Progress: os.Stdout,
+		ReferenceName: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", "ant")),
+		SingleBranch:  true,
+	})
+	if err != nil {
+		fmt.Printf("Error cloning repository: %v\n", err)
+		return
+	}
+
+
+        // Regular expression pattern to match and replace
+	pattern := "template"
+	replace := meta.Name
+
+	// Compile the regular expression pattern
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		fmt.Printf("Error compiling regular expression: %v\n", err)
+		return
+	}
+
+	// Destination directory for copying files (excluding .git)
+	copyDir := "/tmp/" + meta.Namespace + "." + meta.Name
+
+	// Create the copy directory if it doesn't exist
+	err = os.MkdirAll(copyDir, 0755)
+	if err != nil {
+		fmt.Printf("Error creating copy directory: %v\n", err)
+		return
+	}
+
+	// Walk through the cloned repository files and copy them to the copy directory
+	err = filepath.Walk(destDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			if info.Name() == ".git" {
+				// Skip the .git directory
+				return filepath.SkipDir
+			}
+
+			// Create the corresponding directory in the copy directory
+			relPath, err := filepath.Rel(destDir, path)
+			if err != nil {
+				return err
+			}
+			copyPath := filepath.Join(copyDir, relPath)
+			err = os.MkdirAll(copyPath, 0755)
+			if err != nil {
+				return err
+			}
+		} else {
+			if filepath.Base(path) != ".git" {
+				// Read the file contents
+				data, err := ioutil.ReadFile(path)
+				if err != nil {
+					return err
+				}
+
+                 		// Apply the regular expression replacement
+		                data = re.ReplaceAll(data, []byte(replace))
+
+		                fmt.Printf("%v\n", path)
+
+		                // Apply go templates
+		                tmpl, err := template.New("file").Delims("<<", ">>").Parse(string(data))
+		                if err != nil {
+		                	return err
+		                }
+
+		                //var buf strings.Builder
+				var buf bytes.Buffer
+				err = tmpl.Execute(&buf, meta)
+		                if err != nil {
+		                	return err
+		                }
+				err = tmpl.Execute(os.Stdout, meta)
+		                if err != nil {
+		                	return err
+		                }
+
+				// Create the corresponding file in the copy directory
+				relPath, err := filepath.Rel(destDir, path)
+				if err != nil {
+					return err
+				}
+				copyPath := filepath.Join(copyDir, relPath)
+                 		//err = ioutil.WriteFile(copyPath, []byte(buf), 0644)
+                 		err = ioutil.WriteFile(copyPath, buf.Bytes(), 0644)
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error copying files: %v\n", err)
+		return
+	}
+
+        // Remove the cloned repository
+	err = os.RemoveAll(destDir)
+	if err != nil {
+		fmt.Printf("Error removing cloned repository: %v\n", err)
+		return
+	}
+
+	fmt.Println("Repository cloned, files modified, and copied successfully.")
+
+}
---
+++ cmd/root.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "ant",
+	Short: "An ansible toolkit",
+	Long: `
+ANT is a CLI for Ansible written in Go that empowers ansible role creation.
+This application is a tool to generate the needed files
+to quickly create a new ansible role from a template repository.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+            if len(args) == 0 {
+                cmd.Help()
+                os.Exit(0)
+            } 
+	}, 
+}
+
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ant.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
